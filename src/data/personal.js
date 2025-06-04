eaf90d010864b4b7efdfc83f55fd5213f96c63df
mallyaaniruddh <24124133@studentmail.ul.ie>
2025-06-05 00:52:56 +0100

    Fixed resume link to open PDF in new tab

--- src/data/personal.js
+++ src/data/personal.js
@@ -3,7 +3,7 @@ export const personalData = {
   title: "I build solutions that give people their time back.",
   subtitle: "Software Engineer & MSc Candidate",
   description: "with ~2 years of experience developing scalable software solutions. Proficient in Python, C#, and Java, passionate about bridging technical depth with real-world impact through user-focused development.",
-  resumeUrl: "/Aniruddh_Mallya_Resume.pdf", // Add your resume URL
+  resumeUrl: "https://drive.google.com/file/d/1fx-0cNEnfsqGG8CBkRvwYC2eqUlN_mVY/view?usp=drive_link", // Add your resume URL
   social: {
     github: "https://github.com/aniruddh-mallya", // Replace with your GitHub
     linkedin: "https://linkedin.com/in/mallyaaniruddh", // Replace with your LinkedIn
